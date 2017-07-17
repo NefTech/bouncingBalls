dc368e5fb63e38f32cde22e3bbaa184106c20777
NefTech <nefis32@gmail.com>
2017-07-17 14:54:12 -0700

    bouncies

---
+++ main.js
@@ -0,0 +1,100 @@
+
+// setup canvas
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min,max) {
+  var num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+}
+// Construsto function is takinga ball object
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+// with hthis function we are telling the ball to draw itself
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+var testBall = new Ball(50, 100, 4, 4, 'blue', 10);
+testBall.x
+testBall.size
+testBall.color
+testBall.draw()
+
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+var balls = [];
+function loop() {
+  ctx.fillStyle = 'rgba(2, 6, 0, 0.95)';
+  ctx.fillRect(0, 0, width, height);
+
+  while (balls.length < 1137) {
+    var ball = new Ball(
+      random(20,width),
+      random(20,height),
+      random(1,1),
+      random(1,3),
+      'rgb(' + random(0,1500) + ',' + random(0,155) + ',' + random(0,155) +')',
+      random(5,10)
+    );
+    balls.push(ball);
+  }
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+  }
+
+  requestAnimationFrame(loop);
+}
+loop();
+
+Ball.prototype.collisionDetect = function() {
+  for (var j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+}
+
+balls[i].collisionDetect();
